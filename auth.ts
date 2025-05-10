1c66b8eb95f3fcaf7f8e9bac45ebcc71de6b5a3e
Kevinrua2003 <kevinrua2003@gmail.com>
2025-05-10 18:32:16 -0400

    Actualizado el package json, eliminado prisma del proyecto.

--- auth.ts
+++
@@ -1,9 +0,0 @@
-import NextAuth from "next-auth"
-import Google from 'next-auth/providers/google';
-import {prisma} from "./prisma";
-import {PrismaAdapter} from "@auth/prisma-adapter";
-
-export const { handlers, signIn, signOut, auth } = NextAuth({
-    adapter: PrismaAdapter(prisma),
-    providers: [Google],
-})
